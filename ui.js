55604d9346d8225136059a65f1dabaae7ebda637
Vadim Kantorov <vadimkantorov@gmail.com>
2021-01-02 00:32:58 +0200

    ...

--- ui.js
+++ ui.js
@@ -212,7 +212,7 @@ export class Shell
                 {
                 }
                 else if(this.busybox_applets.includes(cmd))
-                    print_or_dump(this.busybox.run([cmd, ...args]).stdout);
+                    print_or_dump(this.busybox.run([cmd, ...args]).stdout, '');
                 else if(cmd == 'tabs')
                     print_or_dump(Object.keys(this.tabs).sort().join('\t'));
                 else if(cmd == 'dirty')

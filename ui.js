4497bf07af44e57ad1a9aed681c255a5c31cdadd
Vadim Kantorov <vadimkantorov@gmail.com>
2021-01-08 21:05:16 +0200

    ...

--- ui.js
+++ ui.js
@@ -227,7 +227,7 @@ export class Shell
 
         for(let cmdline of current_terminal_line.split('&&'))
         {
-            let print_or_dump = (arg, ...args) => arg && this.terminal_print(toString(arg).replace('\n', '\r\n'), ...args);
+            let print_or_dump = (arg, ...args) => arg && this.terminal_print(toString(arg), ...args);
             let redirect = null;
 
             if(cmdline.includes('>>'))

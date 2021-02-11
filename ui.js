62363cc34467fab8c11cf35a53eec2165eb1453e
Vadim Kantorov <vadimkantorov@gmail.com>
2021-02-11 18:53:35 +0200

    ...

--- ui.js
+++ ui.js
@@ -252,7 +252,7 @@ export class Shell
                 return arg.map(toString).join('\t');
             else
             {
-                let res = arg.stdout.replace('\r\n', '\n').replace('\n', '\r\n');
+                let res = arg.stdout.replaceAll('\r\n', '\n').replaceAll('\n', '\r\n');
                 if(res.length > 0 && res[res.length - 1] != '\n')
                     res += '\r\n';
                 return res;

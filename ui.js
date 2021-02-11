c61ed8d103d172efd270223d02fb42fef7e20332
Vadim Kantorov <vadimkantorov@gmail.com>
2021-02-11 20:32:53 +0200

    ...

--- ui.js
+++ ui.js
@@ -423,13 +423,10 @@ export class Shell
         this.FS.writeFile(this.readme_tex, this.readme);
         this.FS.chdir(this.home_dir);
         const sha1_ = uint8array => {
-            const hash_slower = this.busybox.run(['sha1sum'], uint8array).stdout.substring(0, 40);
-            console.log('slower:', hash_slower);
+            //const hash_slower = this.busybox.run(['sha1sum'], uint8array).stdout.substring(0, 40);
             const hash_slow = this.FS.writeFile(this.tmp_file, uint8array) || this.busybox.run(['sha1sum', this.tmp_file]).stdout.substring(0, 40);
-            console.log('slow:', hash_slow);
-            const hash_fast = sha1(uint8array);
-            console.log('fast:', hash_fast);
-            return hash_fast;
+            //const hash_fast = sha1(uint8array);
+            return hashslow;
         };
         this.github = new Github(this.cache_dir, this.merge.bind(this), this.log_big.bind(this), sha1_, this.FS, this.PATH, this);
         

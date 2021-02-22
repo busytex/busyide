e01d23cc38649b9870ce6a1f3fa0047bf3d93f1f
Vadim Kantorov <vadimkantorov@gmail.com>
2021-02-22 19:18:52 +0200

    ...

--- ui.js
+++ ui.js
@@ -121,7 +121,7 @@ export class Shell
         this.ui.filetree.onkeydown = ev => ev.key == 'Enter' || ev.key == ' ' ? this.ui.filetree.ondblclick() : null;
         this.ui.current_file.onclick = () => this.ui.toggle_current_file_rename() || this.ui.current_file_rename.focus();
         this.ui.current_file_rename.onblur = () => this.ui.set_current_file(this.ui.get_current_file()) || this.ui.toggle_current_file_rename();
-        this.ui.remove.onclick = () => this.get_current_file() && this.commands(chain(cmd('rm', '-rf', this.ui.get_current_file()), cmd('open', '.'))); 
+        this.ui.remove.onclick = () => this.ui.get_current_file() && this.commands(chain(cmd('rm', '-rf', this.ui.get_current_file()), cmd('open', '.'))); 
         this.ui.current_file_rename.onkeydown = ev => ev.key == 'Enter' ? (this.mv(this.ui.get_current_file(), this.ui.current_file_rename.value) || this.ui.set_current_file(this.ui.current_file_rename.value) || this.ui.toggle_current_file_rename()) : ev.key == 'Escape' ? ev.target.onblur() : null;
 		
         this.editor.onDidFocusEditorText(ev => this.ui.set_current_file(this.PATH.basename(this.edit_path), this.edit_path, 'editing'));

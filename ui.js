fd288c274947ceba5bf3a3cfa49c4bd2afa85b02
Vadim Kantorov <vadimkantorov@gmail.com>
2021-03-03 18:33:35 +0200

    ..

--- ui.js
+++ ui.js
@@ -261,9 +261,11 @@ export class Shell
             else
             {
                 console.log('Exit code:', arg.exit_code);
+                console.log('Stdout orig:', arg.stdout.length, '[', arg.stdout, ']');
                 let res = arg.stdout.replaceAll('\r\n', '\n').replaceAll('\n', '\r\n');
                 if(res.length > 0 && res[res.length - 1] != '\n')
                     res += '\r\n';
+                console.log('Stdout repl:', res.length, '[', res.stdout, ']');
                 return res;
             };
         };

b00f7fab18f1f3aa341836f1dc4bb9a23b6792ba
Vadim Kantorov <vadimkantorov@gmail.com>
2021-04-06 10:24:15 +0300

    ...

--- github.js
+++ github.js
@@ -51,10 +51,7 @@ export class Github
     {
         const api = realm != 'gists' ? repo_url.replace('github.com', this.PATH.join(this.api_endpoint, realm)) : ('https://' + this.PATH.join(this.api_endpoint, 'gists', this.parse_url(repo_url).reponame));
         const headers = Object.assign({Authorization : 'Basic ' + btoa(this.auth_token), 'If-None-Match' : ''}, body != null ? {'Content-Type' : 'application/json'} : {});
-        return fetch(api + relative_url, Object.assign({method : method || 'get', headers : headers}, body != null ? {body : JSON.stringify(body)} : {})).then(resp => Promise.all([resp[result](), Promise.resolve(resp)])).then( ([res, resp]) => {
-            console.log('result', res, 'resp', resp);
-            Promise.resolve({result : res, ...resp}) 
-        });
+        return fetch(api + relative_url, Object.assign({method : method || 'get', headers : headers}, body != null ? {body : JSON.stringify(body)} : {})).then(resp => resp[result]().then(data => {result : res, ...resp}));
     }
 
     parse_url(repo_url)

ba92233f742de38fe6054665ea2079e0e4b8f957
Vadim Kantorov <vadimkantorov@gmail.com>
2021-05-17 16:32:04 +0300

    ...

--- ui.js
+++ ui.js
@@ -334,6 +334,7 @@ export class Shell
                 console.log('CMD ARGS', cmd, args);
                 args = args.map(a => this.expandcollapseuser(a));
                 args = args.map(a => a.startsWith('"') ? a.slice(1) : a).map(a => a.endsWith('"') ? a.slice(0, a.length - 1) : a);
+                console.log('CMD ARGS 2', cmd, args);
 
                 cmds.push({cmd : cmd, args : args, stdout_redirect : stdout_redirect, stdout_redirect_append : stdout_redirect_append, subcommand : false});
             }

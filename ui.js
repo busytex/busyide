17a3ad629950a7b10c4b4b900564e2a13ece7a41
Vadim Kantorov <vadimkantorov@gmail.com>
2021-06-03 13:28:29 +0300

    ...

--- ui.js
+++ ui.js
@@ -20,7 +20,7 @@ class DataPackageSelector
     
     async find_required_data_packages_js(files)
     {
-        const texmf_packages = new Set(files.filter(f => f.path.startsWith('texmf/texmf-dist/')).map(this.extract_tex_package_name.bind(this)));
+        const texmf_packages = new Set(files.filter(f => f.path.startsWith('texmf/texmf-dist/')).map(f => this.extract_tex_package_name(f.path)));
         
         const tex_packages = new Set(files.filter(f => typeof(f.contents) == 'string').map(f => f.contents.split('\n').filter(l => l.trim()[0] != '%' && l.trim().startsWith('\\usepackage')).map(l => Array.from(l.matchAll(this.regex_usepackage)).filter(groups => groups.length >= 2).map(groups => groups.pop().split(',')  )  )).flat().flat().flat().filter(tex_package => !texmf_packages.has(tex_package)));
 

1eae1841efae96ac87d2a565b88410565d8ba995
Vadim Kantorov <vadimkantorov@gmail.com>
2021-06-27 12:24:16 +0300

    ...

--- ui.js
+++ ui.js
@@ -1091,8 +1091,8 @@ export class Shell
 
                     this.ls_la(abspath, file_path);
                     
-                    //if(file_path != '.')
-                    //    open_editor_tab('');
+                    if(file_path != '.')
+                        open_editor_tab('');
                     
                     file_path = null;
                     return;
@@ -1372,7 +1372,7 @@ export class Shell
         this.ui.update_tex_paths(project_dir ? files.filter(f => f.path.endsWith('.tex')) : [], selected_file_path);
         this.ui.set_project_name(project_dir ? this.PATH.basename(project_dir) : 'N/A');
 
-        if(!this.exists(this.edit_path))
+        if(this.edit_path && !this.exists(this.edit_path))
         {
             console.log('refresh', 'does not exist', '[', this.edit_path, ']');
             if(this.tab)

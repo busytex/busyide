1ef5ce8dc4a1090cb71e079d5c92a8e5368fadbf
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-06 20:03:51 +0200

    ..

--- busybox.js
+++ busybox.js
@@ -31,6 +31,7 @@ export class Busybox
             instantiateWasm(imports, successCallback)
             {
                 WebAssembly.instantiate(wasm_module, imports).then(successCallback);
+                return {};
             },
             
             stdin()

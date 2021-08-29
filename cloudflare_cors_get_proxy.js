d4be186e1f5abcc2cf3fdca199c6933ab0713545
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-29 16:50:05 +0200

    Update cloudflare_cors_get_proxy.js

--- cloudflare_cors_get_proxy.js
+++ cloudflare_cors_get_proxy.js
@@ -4,16 +4,15 @@ addEventListener('fetch', event => event.respondWith((async () =>
 {
     
     const url = new URL(event.request.url);
-    const origin = event.request.headers.get('Origin');
+    const origin = event.request.headers.get('Origin'), user_agent = event.request.headers.get('User-Agent');
 
     if (url.search.startsWith('?'))
     {
         const url_href = unescape(unescape(url.search.substr(1)));
 
-        const response = await fetch(url_href, { method: 'GET' } );
+        const response = await fetch(url_href, { method: 'GET', headers : {'User-Agent' : user_agent } } );
 
         const headers = new Headers(response.headers);
-
         headers.set('Access-Control-Allow-Origin', origin);
 
         return new Response(response.body, { status: response.status, headers: headers });

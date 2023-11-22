2ce187640a8864693916d66e609916375c683922
Vadim Kantorov <vadimkantorov@gmail.com>
2023-11-22 16:50:01 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,28 +1,22 @@
-# -*- coding: utf-8 -*-
-#test on python 3.4 ,python of lower version  has different module organization.
 import http.server
 import socketserver
 
+Handler.extensions_map = {
+    '.manifest': 'text/cache-manifest',
+    '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'image/svg+xml',
+    '.css': 'text/css',
+    '.js': 'application/x-javascript',
+    '.wasm': 'application/wasm',
+    '.tex': 'application/x-tex',
+    '': 'application/octet-stream', # default
+}
+
 PORT = 8080
-
 Handler = http.server.SimpleHTTPRequestHandler
-
-Handler.extensions_map={
-        '.manifest': 'text/cache-manifest',
-	'.html': 'text/html',
-        '.png': 'image/png',
-	'.jpg': 'image/jpg',
-	'.svg':	'image/svg+xml',
-	'.css':	'text/css',
-	'.js':	'application/x-javascript',
-	'.wasm': 'application/wasm',
-        '.tex': 'application/x-tex',
-	'': 'application/octet-stream', # Default
-    }
-
-Server = http.server.ThreadingHTTPServer
-#Server = socketserver.TCPServer
+Server = http.server.ThreadingHTTPServer #socketserver.TCPServer
 httpd = Server(("", PORT), Handler)
-
 print("serving at port", PORT)
 httpd.serve_forever()
